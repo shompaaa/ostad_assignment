06756cdbaa09c698227d2ec189e188dd06e6fe51
Shompa Akter <shompaakter334@gmail.com>
2025-01-01 23:11:54 +0600

    all done!

--- bin/dart_liveTest-1.dart
+++ bin/dart_liveTest-1.dart
@@ -2,8 +2,8 @@ main(){
   print("Triangle Area: ${triangle(29, 25)}");
 }
 
-
+//Creating triangle area function
 double triangle(base, height){
-  double area = 0.5 * base * height;
+  double area = 0.5 * base * height; //area calculation formulae
   return area;
 }
